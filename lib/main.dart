f3c1952868ba0c5579e7b80a518da4025e0c3522
Azeezmariam <m.azeez@alustudent.com>
2024-06-17 00:00:14 +0200

    animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:lets_route/pages/first_page.dart';
+import 'package:lets_route/pages/first_page.dart' as first;
 import 'package:lets_route/pages/home_page.dart';
+import 'package:lets_route/pages/details_page.dart'; // Import the details page
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.blueGrey,
-        primaryColor: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.brown,
+        primaryColor: Colors.greenAccent,
         appBarTheme: const AppBarTheme(
           backgroundColor: Color(0xFF004B5C),
           titleTextStyle: TextStyle(
@@ -37,10 +38,12 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const FirstPage(),
+      initialRoute: '/homepage',
       routes: {
-        '/firstpage': (context) => const FirstPage(),
+        '/firstpage': (context) => const first.FirstPage(),
         '/homepage': (context) => const HomePage(),
+        '/details': (context) =>
+        const DetailsPage(), // Add the details page route
       },
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,99 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+
+//A Floating screen animation//
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = Tween<Offset>(
+      begin: Offset(0, 0),
+      end: Offset(0, -0.1),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('Go to First Page'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/firstpage');
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Go to First Page'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/firstpage');
+              },
+            ),
+            const SizedBox(height: 20),
+            SlideTransition(
+              position: _animation,
+              child: Container(
+                width: 400,
+                height: 300,
+                decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.lightBlue.withOpacity(0.2),
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    'Floating Screen',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5, // Number of product items
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text('Product ${index + 1}'),
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/details',
+                        arguments: {'productName': 'Product ${index + 1}'},
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
